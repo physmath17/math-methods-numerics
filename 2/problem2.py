f4ae634b8e31968bc16449ddea32f8e08d29a7c1
Krishh <krishnajalan.nitr@gmail.com>
2020-12-26 02:59:12 +0530

    second problem

---
+++ 2/problem2.py
@@ -0,0 +1,54 @@
+'''
+First part of the problem - solve the Airy equation and plot the results
+Second part of the problem - use series soulution to approximate the Airy function and plot the approximation along with the original function
+'''
+
+from scipy.special import airy, gamma
+import matplotlib.pyplot as plt
+import numpy as np
+from math import exp
+
+# parameters
+a = 0 
+y0 = 1/(pow(3, 2/3)*gamma(2/3))
+dy0 = -1/(pow(3, 1/3)*gamma(1/3))
+
+# solving the Airy equation using central difference
+def cntrl(xi, xf, yi, dyi, steps) :
+    ''' solves the Airy equation y''(x) = xy
+    xi : initial x datapoint, xf : final x datapoint, yi : initial y datapoint, dyi : derivativative initial data, steps : number of iterations,
+    returns 2D array of x, y values '''
+
+    h = (xf - xi)/steps
+    x = np.linspace(xi, xf, steps)
+    y = np.array([yi])
+
+    # using disrete derivative implementation of the continuous derivative
+    y1 = (h*dyi + yi)
+    y = np.append(y, y1)
+    for i in range(0, len(x)-2) :
+        dy = (2 + x[i]*h**2)*y[i + 1] - y[i]
+        y = np.append(y, dy)
+
+    return x, y
+
+xg, yg = cntrl(a, 5, y0, dy0, 500)       # x range greater than 0
+xl, yl = cntrl(a, -15, y0, dy0, 1500)    # x range less than 0
+
+x = np.append(xl[::-1], xg[1::])
+y = np.append(yl[::-1], yg[1::])
+ai, aip, bi, bip = airy(x)
+
+# approximating the Airy equation with the series solution
+
+
+# for i in range(len(x)) :
+#     print(x[i], y[i])
+
+plt.plot(x, ai, 'r', label='Ai(x)')
+plt.plot(x, y, 'b', label='y(x)')
+# plt.plot(x, bi, 'b--', label='Bi(x)')
+plt.ylim(-0.5, 1.0)
+plt.grid()
+plt.legend(loc='upper left')
+plt.show()
