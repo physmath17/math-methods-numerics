0ebe020e81df1feff8232a3d54ed8e68f022adb0
Krishh <krishnajalan.nitr@gmail.com>
2020-12-28 19:15:31 +0530

    irst part done

--- 2/problem2.py
+++ 2/problem2.py
@@ -1,22 +1,25 @@
 '''
 First part of the problem - solve the Airy equation and plot the results
-Second part of the problem - use series soulution to approximate the Airy function and plot the approximation along with the original function
+Second part of the problem - use series soulution to approximate the Airy function at x = -10000, -100, -1, 1, 100, 10000
 '''
 
 from scipy.special import airy, gamma
 import matplotlib.pyplot as plt
 import numpy as np
-from math import exp
+from math import factorial as fact
 from datetime import datetime
 
 startTime = datetime.now()
 
 # parameters
 a = 0 
-y0 = 1/(pow(3, 2/3)*gamma(2/3))
-dy0 = -1/(pow(3, 1/3)*gamma(1/3))
+# y0 = 1/(pow(3, 2/3)*gamma(2/3))         # for Ai(x)
+# dy0 = -1/(pow(3, 1/3)*gamma(1/3))       # for Ai(x)
+y0 = 1/(pow(3, 1/6)*gamma(2/3))         # for Bi(x)
+dy0 = pow(3, 1/6)/gamma(1/3)            # for Bi(x)
 
 # solving the Airy equation using central difference
+"""
 def cntrl(xi, xf, yi, dyi, steps) :
     ''' solves the Airy equation y'' = xy
     xi : initial x datapoint, xf : final x datapoint, yi : initial y datapoint, dyi : derivativative initial data, steps : number of iterations,
@@ -54,24 +57,67 @@ def frwrd(xi, xf, yi, dyi, steps) :
 
     return x, y
 
-xg, yg = cntrl(a, 5, y0, dy0, 5000)      # x range greater than 0
+xg, yg = cntrl(a, 2, y0, dy0, 1000)      # x range greater than 0
 xl, yl = cntrl(a, -15, y0, dy0, 1500)    # x range less than 0
 
 x = np.append(xl[::-1], xg[1::])
 y = np.append(yl[::-1], yg[1::])
-ai, aip, bi, bip = airy(x)
+"""
+v = [-10000, -100, -1, 1, 100, 10000]
+ai, aip, bi, bip = airy(v)
 
 # approximating the Airy equation with the series solution
+def airy_first(x, y) :
+    ''' returns the number of terms required in the power series to approximate the Airy function of the first kind at x and and the value of Ai(x) '''
+
+    s = 0.0
+    n = 0
+    while True :
+        if n%3 == 0 :
+            s += pow(3, -2/3)*pow(x, n)/(pow(3, 2*n/3)*fact(n/3)*gamma(n/3 + 2/3))  
+        elif n%3 == 1 :
+            s += -pow(3, -4/3)*pow(x, n)/(pow(3, 2*(n-1)/3)*fact((n-1)/3)*gamma(n/3 + 1))
+        else:
+            s += 0
+
+        if abs(s - y) < 1e-3 :
+            break
+        n += 1
+    return n, s
+
+def airy_second(x, y) :
+    ''' returns the number of terms required in the power series to approximate the Airy function of the first kind at x and and the value of Ai(x) '''
+
+    s = 0.0
+    n = 0
+    while True :
+        s += pow(3, -1/6)*pow(x, n)/(pow(3, 2*n/3)*fact(n/3)*gamma(n/3 + 2/3))*(n%3 == 0) - pow(3, -5/6)*pow(x, n)/(pow(3, 2*(n-1)/3)*fact((n-1)/3)*gamma(n/3 + 1))*(n%3 == 1) + 0*(n%3 == 2)
+
+        if abs(s - y) < 1e-3 :
+            break
+        n += 1
+    return n, s
+x=1
+s=0
+for n in range(600) :
+    if n%3 == 0 :
+            s += pow(3, -2/3)*pow(x, n)/(pow(3, 2*n/3)*fact(n/3)*gamma(n/3 + 2/3))  
+    elif n%3 == 1 :
+        s += -pow(3, -4/3)*pow(x, n)/(pow(3, 2*(n-1)/3)*fact((n-1)/3)*gamma(n/3 + 1))
+    else:
+        s += 0
+print(s, ai[4])
 
 endTime = datetime.now()
 print("Execution time : ", endTime - startTime)
 
-plt.plot(x, ai, 'r', label='Ai(x)')
-plt.plot(x, y, 'b', label='y(x)')
+# plt.plot(x, ai, 'r', label='Ai(x)')
 # plt.plot(x, bi, 'r', label='Bi(x)')
-plt.ylim(-0.5, 0.6)
-plt.xlabel("x")
-plt.title("Airy equation solution using central difference method")
-plt.grid()
-plt.legend(loc='upper left')
-plt.show()
+# plt.plot(x, y, 'b', label='y(x)')
+# plt.ylim(-0.5, 0.6)             # for Ai(x)
+# plt.ylim(-0.6, 4)                 # for Bi(x)
+# plt.xlabel("x")
+# plt.title("Airy equation solution using central difference method")
+# plt.grid()
+# plt.legend(loc='upper left')
+# plt.show()
