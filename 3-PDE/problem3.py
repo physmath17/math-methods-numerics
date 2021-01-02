87f9be986b1b692a5d7ff4dc0bd87ee8e88fe01f
Krishh <krishnajalan.nitr@gmail.com>
2021-01-03 01:41:32 +0530

    3-heat equation

---
+++ 3-PDE/problem3.py
@@ -0,0 +1,8 @@
+'''
+solving the heat equation in 1D ---- (take D = 1)
+u_t - D*u_xx = 0, with 0 < x < 1, t > 0, 
+u(0, t) = 0 = u(1, t), 
+u(x, 0) = phi(x) = 2*x       if 0 < x <= 0.5
+                 = 2*(1 - x) if 0.5 <= x < 1.
+'''
+
--- 4 Ising/sir_code/ran3.h
+++
@@ -1,4 +0,0 @@
-#ifndef ran3_H_GUARD
-#define ran3_H_GUARD
-double ran3(long int *idum);
-#endif
