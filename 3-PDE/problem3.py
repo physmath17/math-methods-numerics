96853ebe873dd008b8e6ad49c6a2dd7d2501ccc5
Krishh <krishnajalan.nitr@gmail.com>
2021-01-04 15:46:51 +0530

    heat equation solved

--- 3-PDE/problem3.py
+++ 3-PDE/problem3.py
@@ -18,52 +18,52 @@ hx = 0.05
 ht = 0.0025
 
 # initial conditions specfied by
-phi = lambda y : ((0 < y <= 0.5 and 2*y) or 2*(1 - y))
+phi = lambda y : 2*y*(y<0.5) + 2*(1-y)*(y>=0.5)
 
 # solving the equation
-def solver(phi, xi, xf, dx, dt, s=1, D=1) :
+def solver(phi, xi, xf, dx, dt=0.0025, s=1, D=1) :
     ''' D : diffusion constant, xi, xf : spatial boundary points, dx : spatial increment, dt : time increment, s = dt/(dx)^2
     returns a 2D array as solution where the rows for a fixed column specify solutions at different times and the columns for a fixed row specify the solution at different spatial points '''
     if s != 1 :
         dt = s*(dx**2)/D
+    else :
+        s = D*dt/(dx**2)
 
-    s = D*dt/(dx**2)
-
-    Nx = int((xf-xi)/dx)
-    Nt = 1000
+    Nx = int((xf-xi)/dx) + 1
+    Nt = 301
 
     x = np.linspace(xi, xf, Nx)
-    t = np.array([i*dt for i in range(Nt)])
     u = np.empty((Nt, Nx))
 
     # initial condition
     # for t there is one initial condition is specified by phi
-    for i in range(1, len(x)-1) :
+    for i in range(Nx) :
         u[0][i] = phi(x[i])
     
     # for x we need to specify two initial conditions
-    u[:,0] = 0
+    for i in range(1, Nt) :
+        u[i][0] = 0
+        u[i][Nx-1] = 0
 
     # difference equation - recurrence relation for the heat equation
-    for time in range(Nt-1) :
+    for time in range(1, Nt) :
         for space in range(1, Nx-1) :
             u[time][space] = s*(u[time-1][space+1] + u[time-1][space-1]) + (1 - 2*s)*u[time-1][space]
-    return u
+    return x, u, dt
 
-# def heatmap(v, dt, k) :
-#     plt.clf()
-#     plt.title(f"Temperature at t = {k*dt:.3f} unit time")
-#     plt.xlabel("x")
-#     plt.pcolormesh(v, cmap=plt.cm.jet, vmin=0, vmax=100)
-#     plt.colorbar()
+def heatmap(x, v, dt, k) :
+    plt.clf()
+    plt.title(f"Temperature at t = {k*dt:.3f} unit time")
+    plt.xlabel("x")
+    plt.ylabel("u")
+    plt.plot(x, v)
+    return plt
 
-#     return plt
+x, u, delta_t = solver(phi, a, b, hx, s=0.51)
 
-T = solver(phi, a, b, hx, ht, s=0.51)
-print(T)
+def animate(k) :
+    return heatmap(x, u[k], delta_t, k)
 
-# def animate(k) :
-#     heatmap(T[k], ht, k)
-
-# anim = animation.FuncAnimation(plt.figure(), animate, interval=1, frames=1000, repeat=False)
-# anim.save("heat_equation_solution.gif")
+fig, ax = plt.subplots()
+anim = animation.FuncAnimation(fig, animate, interval=200, frames=300, repeat=False)
+anim.save("heat_equation_solution_s_0.51.gif", writer='imagemagick', fps=10)
