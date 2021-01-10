762fc43dd8f8ef9f9b2e65ac9566020f87cbc5d2
Krishh <krishnajalan.nitr@gmail.com>
2021-01-10 16:42:56 +0530

    done with numerics, visualization for later

--- 4-Ising/my_code/magnetisation_analysis.py
+++ 4-Ising/my_code/magnetisation_analysis.py
@@ -29,12 +29,12 @@ import matplotlib.pyplot as plt
 
 for v in range(1, 12) :
     plt.clf()
-    for i in range(10, 61, 10) :
-        data = np.loadtxt("data_L_{}.txt".format(i))
-        plt.plot((i**(1/v))*(data[:,0] - 0.48), abs(data[:,2]), label=r"{} $\times$ {}".format(i, i))
+    for i in range(30, 61, 10) :
+        data = np.loadtxt("data_L_modified_{}.txt".format(i))
+        plt.plot((i**(1/v))*(data[:,0] - 0.46), abs(data[:,2]), label=r"{} $\times$ {}".format(i, i))
     plt.legend()
     plt.grid(True)
     plt.title(r"M $\times$ $L^0$ vs $L^{1/\, %d}\beta$J" % v + "\n")
     plt.xlabel(r"$L^{1/%d}\beta$J" % v)
     plt.ylabel(r"M $\times$ $L^0$")
-    plt.savefig("critical_exponent/critical_exponent_500_{p:.2f}.png".format(p=v))
+    plt.savefig("critical_exponent/critical_exponent_2000_{p:.2f}.png".format(p=v))
